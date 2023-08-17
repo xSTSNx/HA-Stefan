81d3b645db250fb0d84d42a7ac8d2ff9a43d0482
Stefan <115979341+xSTSNx@users.noreply.github.com>
2023-08-17 13:36:28 +0200

    Add files via upload

---
+++ test_run.py
@@ -0,0 +1,17 @@
+import unittest
+
+from run import multi
+
+
+class TestSub(unittest.TestCase):
+
+    def test_multi_function(self):
+        self.assertEqual(multi(3, 7), 21)
+        self.assertEqual(multi(4, 6), 24)
+
+    def test_multi_function_with_floats(self):
+        self.assertAlmostEqual(multi(7.5, 7.5), 15.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
